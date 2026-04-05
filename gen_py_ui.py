8097850ca8ac90452f0a549718294e7ea7432f1f
ExRFy <3657936745@qq.com>
2026-04-05 14:29:57 +0800

    把UI文件移动到UI文件夹

--- gen_py_ui.py
+++ gen_py_ui.py
@@ -1,6 +1,6 @@
 import os
 if os.path.exists('ui') == False:
     os.mkdir('ui')
-os.system('pyside6-uic main.ui -o ui/main.py')
-os.system('pyside6-uic pwd.ui -o ui/pwd.py')
-os.system('pyside6-uic create_pwd.ui -o ui/create_pwd.py')
+os.system('pyside6-uic ui/main.ui -o ui/main.py')
+os.system('pyside6-uic ui/pwd.ui -o ui/pwd.py')
+os.system('pyside6-uic ui/create_pwd.ui -o ui/create_pwd.py')
